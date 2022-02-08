a1985fe847e546d9fc650f5673a69b8572945c81
chan-fullstack <chan@fullstack.ph>
2022-02-08 18:56:38 +0800

    Implement Slug System on Raket API

---
+++ src/api/raket/content-types/raket/lifecycles.js
@@ -0,0 +1,18 @@
+const slugify = require('slugify');
+
+module.exports = {
+  async beforeCreate(event) {
+    const { data } = event.params;
+
+    if (data.title) {
+      data.slug = slugify(data.title, {lower: true});
+    }
+  },
+  async beforeUpdate(event) {
+    const { data } = event.params;
+
+    if (data.title) {
+      data.slug = slugify(data.title, {lower: true});
+    }
+  },
+};
---
+++ src/api/raket/routes/raket.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * raket router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::raket.raket');
